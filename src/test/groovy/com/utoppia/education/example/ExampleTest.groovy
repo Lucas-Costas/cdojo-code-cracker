eb1d945831c539ad13f18b341f53836d03f3734d
diego-utoppia <dbaez@utoppia.com>
2023-01-20 17:47:07 -0300

    Add decoder test

--- src/test/groovy/com/utoppia/education/example/ExampleTest.groovy
+++ src/test/groovy/com/utoppia/education/example/ExampleTest.groovy
@@ -13,4 +13,16 @@ class ExampleTest extends Specification {
             message == "Hi I am an example"
     }
 
+    def "Decode an example string"(){
+        given: "A string to decode"
+        def toDecode = "jidee>!"
+        Example exam = new Example()
+
+        when: "It's decoded"
+        def decoded = exam.decode(toDecode)
+
+        then: "It's decoded correctly"
+        decoded == "utoppia"
+    }
+
 }
